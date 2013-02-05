bed3b3ee429dc84768cd4f23bd9f2522ed8d8445
Karl Gustav Georgsen <rinpoche4@gmail.com>
2013-02-05 23:06:45 +0100

    CheckMousePress
    En klasse som sjekker om musen klikker på et rektangel. Kalles på ved:
    CheckMousePress.IsBeingPressed(rektangel), returnerer true eller false.

---
+++ Ladybug Mayhem/Ladybug Mayhem/Ladybug Mayhem/CheckMousePress.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Ladybug_Mayhem
+{
+    class CheckMousePress
+    {
+        public static bool IsBeingPressed(Rectangle rectangle)
+        {
+            if (GlobalVars.MOUSE_STATE.LeftButton == ButtonState.Pressed && GlobalVars.PREVIOUS_MOUSE_STATE.LeftButton == ButtonState.Released && rectangle.Contains(new Point(GlobalVars.MOUSE_STATE.X, GlobalVars.MOUSE_STATE.Y)))
+                return true;
+            return false;
+        }
+    }
+}
