2f0464b5d947b4135deed0e2fd6a2de0dff3845a
Karl Gustav Georgsen <geokar12@gmail.com>
2013-02-05 14:19:26 +0100

    No more extensions!
    FallingObject er nå en egen klasse, istedenfor en subklasse for
    DrawObject

--- Ladybug Mayhem/Ladybug Mayhem/Ladybug Mayhem/FallingObject.cs
+++ Ladybug Mayhem/Ladybug Mayhem/Ladybug Mayhem/FallingObject.cs
@@ -7,24 +7,31 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Ladybug_Mayhem
 {
-    class FallingObject : DrawObject
+    class FallingObject
     {
         public bool falling;
         public int ySpeed;
         public int currentYPos { get; protected set; }
         private int prevYPos;
+        private Texture2D sprite;
+        public int height { get; protected set; }
+        public int width { get; protected set; }
+        private Vector2 drawPlacement;
         public FallingObject(Game game, ContentManager content, String receivedSprite, int drawAmount, Vector2 drawPlacement, bool falling, int ySpeed)
-            : base(game, content, receivedSprite, drawAmount, drawPlacement)
         {
             this.ySpeed = ySpeed;
             this.falling = falling;
             currentYPos = (int)drawPlacement.Y;
+            sprite = content.Load<Texture2D>(receivedSprite);
+            height = sprite.Bounds.Height;
+            width = sprite.Bounds.Width;
+            this.drawPlacement = drawPlacement;
         }
         public void Update(GameTime gameTime)
         {
             if (falling) currentYPos += ySpeed;
         }
-        public override void Draw(SpriteBatch spriteBatch)
+        public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(sprite, new Vector2(drawPlacement.X, currentYPos), Color.White);
 
