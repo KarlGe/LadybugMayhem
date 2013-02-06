c7840b67ea585f0d2f59883fadcca385906358e9
Karl Gustav Georgsen <geokar12@gmail.com>
2013-02-06 18:55:07 +0100

    SexyandIknowit

--- Ladybug Mayhem/Ladybug Mayhem/Ladybug Mayhem/Citizen.cs
+++ Ladybug Mayhem/Ladybug Mayhem/Ladybug Mayhem/Citizen.cs
@@ -17,34 +17,57 @@ namespace Ladybug_Mayhem
 
         private Texture2D _sprite;
 
+        private Rectangle _clickableRectangle;
+
+        private int _timeKeeper;
+        private int _randomTimeKeeper;
+        private int _spriteNumber;
+
+        private float _randomSpeedBoost;
         private float _speed;
 
-        private Rectangle _destinationRectangle;
-
-        public Citizen(ContentManager content, int citizenNumber)
+        public Citizen(ContentManager content)
         {
-            _citizenNumber = citizenNumber;
-            _sprite = content.Load<Texture2D>("Character Boy");
-            _destinationRectangle = new Rectangle(-200 - (300 * _citizenNumber), (int)GlobalVars.CITIZEN_SPAWN_POS.Y,
+            _spriteNumber = GlobalVars.RAND.Next(GlobalVars.CITIZEN_SPRITE_NAME.Length);
+            _sprite = content.Load<Texture2D>(GlobalVars.CITIZEN_SPRITE_NAME[_spriteNumber]);
+            _clickableRectangle = new Rectangle(-300, (int)GlobalVars.CITIZEN_SPAWN_POS.Y,
                 GlobalVars.CITIZEN_BOX_WIDTH, GlobalVars.CITIZEN_BOX_HEIGHT);
             _speed = 3;
+            _timeKeeper = 0;
+            _randomTimeKeeper = 0;
         }
 
-        public void Update()
+        public void Update(GameTime gameTime)
         {
-            _destinationRectangle.X += (int)_speed;
+            _randomTimeKeeper += gameTime.ElapsedGameTime.Milliseconds;
+            if (_randomTimeKeeper >= 1000 && _clickableRectangle.X < (0 - _clickableRectangle.Width) && _clickableRectangle.X > -3000)
+            {
+                _randomSpeedBoost = (float)(GlobalVars.RAND.NextDouble() / 5);
+                _speed += _randomSpeedBoost;
+                _randomTimeKeeper = 0;
+                Console.WriteLine(_speed);
+            }
+            _timeKeeper += gameTime.ElapsedGameTime.Milliseconds;
+            if (_timeKeeper >= 20000 && _clickableRectangle.X < (0-_clickableRectangle.Width) && _clickableRectangle.X > -3000)
+            {
+                _speed += 1;
+                _timeKeeper = 0;
+            }
+            _clickableRectangle.X += (int)_speed;
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(_sprite, new Vector2(_destinationRectangle.X, _destinationRectangle.Y), GlobalVars.CITIZEN_SOURCE_RECTANLGE,
-                Color.White);
+            spriteBatch.Draw(_sprite, new Vector2(_clickableRectangle.X, _clickableRectangle.Y), GlobalVars.CITIZEN_SPRITE_RECTANGLE,
+                Color.White, 0, Vector2.Zero, 1, SpriteEffects.None, 1);
         }
 
         //jeg fikk feilmelding på _spriteBounds.X i Update() dersom jeg ga _spriteBounds en get;-set; ..? Derfor lager jeg denne..
         public Rectangle GetCitizenBox()
         {
-            return _destinationRectangle;
+            _clickableRectangle.X = (int)GlobalVars.CITIZEN_SPAWN_POS.X;
+            _clickableRectangle.Y = (int)GlobalVars.CITIZEN_SPAWN_POS.Y;
+            return new Rectangle
         }
 
         /// <summary>
