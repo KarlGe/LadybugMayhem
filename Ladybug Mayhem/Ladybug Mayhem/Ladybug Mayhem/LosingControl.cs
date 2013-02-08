c01187688e563202d29f8021667f3c47511ae057
Adrian Arnason <struckthenerve@gmail.com>
2013-02-08 14:59:05 +0100

    Småtweak2
    Fjernet konsollskrift

--- Ladybug Mayhem/Ladybug Mayhem/Ladybug Mayhem/LosingControl.cs
+++ Ladybug Mayhem/Ladybug Mayhem/Ladybug Mayhem/LosingControl.cs
@@ -28,8 +28,8 @@ namespace Ladybug_Mayhem
             _content = content;
             spawnTimeLevels = new int[3];
             spawnTimeLevels[0] = 0;
-            spawnTimeLevels[1] = 3000;
-            spawnTimeLevels[2] = 3500;
+            spawnTimeLevels[1] = 3500;
+            spawnTimeLevels[2] = 4000;
             _drawHearts = new List<DrawSprite>();
             _citizenList = new List<Citizen>();
             Reset(content);
@@ -38,9 +38,7 @@ namespace Ladybug_Mayhem
         public static void Update(GameTime gameTime, GameWindow window)
         {
             _spawnTimer += gameTime.ElapsedGameTime.Milliseconds;
-            Console.WriteLine(GlobalVars.BUGS_KILLED);
             minSpawnTime = spawnTimeLevels[GlobalVars.BUGS_KILLED] + _spawnTimer;
-            Console.WriteLine(_spawnTimer);
             if (minSpawnTime >= 5000 && _populationCount < GlobalVars.MAX_CITIZENS)
             {
                 _citizenList.Add(new Citizen(_content, _populationCount));
