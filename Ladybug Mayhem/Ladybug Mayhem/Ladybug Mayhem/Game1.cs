5f5cfe2cc26b60e30c04fec3f35003bb853b9bec
Adrian Arnason <struckthenerve@gmail.com>
2013-02-08 21:11:55 +0100

    Game1 kommentert

--- Ladybug Mayhem/Ladybug Mayhem/Ladybug Mayhem/Game1.cs
+++ Ladybug Mayhem/Ladybug Mayhem/Ladybug Mayhem/Game1.cs
@@ -16,15 +16,15 @@ namespace Ladybug_Mayhem
     /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
+        //Deklarerer nødvendige objekter.
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         GameOverScreen gameOverScreen;
         StartScreen startScreen;
         DrawBG backgroundScreen;
         LadybugAndGemLogic ladybugs;
-        private bool gameOver = false;
-        int screenWidth;
-        int screenHeight;
+        //Privat boolean for å holde styr på om spillet er over (vunnet eller tapt)
+        private bool _gameOver = false;
 
         public Game1()
         {
@@ -41,19 +41,24 @@ namespace Ladybug_Mayhem
         /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
             base.Initialize();
+            //Musepeker er synlig
             this.IsMouseVisible = true;
-            screenHeight = Window.ClientBounds.Height;
-            screenWidth = Window.ClientBounds.Width;
+            //Musens status ligger i GlobalVars (der kan den hentes ut av alle)
             GlobalVars.MOUSE_STATE = Mouse.GetState();
+            //Objektet som tegner bakgrunnen
             backgroundScreen = new DrawBG(this);
+            //Objektet som tegner start-skjermen
             startScreen = new StartScreen(this, Content);
+            //Objektet som tegner slutt-skjermen (vunnet eller tapt)
             gameOverScreen = new GameOverScreen(this);
 
+            //Klassen som styrer hjerter og innbyggere, holder orden på om du taper
             LosingControl.Initialize(Content);
+            //Objektet som styrer diamanter of marihøner, holder orden på om du vinner
             ladybugs = new LadybugAndGemLogic(this.Content, spriteBatch);
-            base.Initialize(); 
+
+            //Er det objekter eller bare "vanlige" klasser som er riktig måte å lage kontroller-klasser (slik som over) på?
         }
 
         /// <summary>
@@ -64,9 +69,6 @@ namespace Ladybug_Mayhem
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            //font = Content.Load<SpriteFont>("TestFont");
-
-            // TODO: use this.Content to load your game content here
         }
 
         /// <summary>
@@ -75,7 +77,6 @@ namespace Ladybug_Mayhem
         /// </summary>
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
         }
 
         /// <summary>
@@ -85,33 +86,33 @@ namespace Ladybug_Mayhem
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            gameOver = GlobalVars.lives <= 0 || GlobalVars.gems >= GlobalVars.MAX_GEMS;
+            _gameOver = GlobalVars.lives <= 0 || GlobalVars.gems >= GlobalVars.MAX_GEMS;
             // Allows the game to exit
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
 
             base.Update(gameTime);
+            //Oppdaterer musens status GlobalVars
             GlobalVars.PREVIOUS_MOUSE_STATE = GlobalVars.MOUSE_STATE;
             GlobalVars.MOUSE_STATE = Mouse.GetState();
             //Starskjerm
             if(startScreen.draw) 
                 startScreen.Update(gameTime);
             //SPILL!
-            if (!startScreen.draw && !gameOver)
+            if (!startScreen.draw && !_gameOver)
             {
                 LosingControl.Update(gameTime, Window);
                 ladybugs.Update(gameTime);
             }
-            //GameOver-skjerm
-            if (gameOver) gameOverScreen.Update(gameTime);
+            //Sluttskjerm
+            if (_gameOver) gameOverScreen.Update(gameTime);
 
-            //Hvis man ønsker å spille igjen
+            //Hvis man ønsker å spille igjen kalles "reset" funksjonen i hver av kontroller-klassene
             if (gameOverScreen.replay)
             {
                 gameOverScreen.reset();
                 LosingControl.Reset(Content);
                 ladybugs.Reset();
-
                 startScreen.reset();
             }
         }
@@ -122,21 +123,23 @@ namespace Ladybug_Mayhem
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
+            //Bakgrunnsfarge
             GraphicsDevice.Clear(Color.CornflowerBlue);
-            // TODO: Add your drawing code here
+
+            //Start å tegne, sorter etter layerDepth (+ en BlendStat som fungerer)
             spriteBatch.Begin(SpriteSortMode.FrontToBack, BlendState.NonPremultiplied);
             //Bakgrunn
             backgroundScreen.Draw(spriteBatch);
             //Startskjerm
             if(startScreen.draw) startScreen.Draw(spriteBatch);
             //SPILL!
-            if (!startScreen.draw && !gameOver)
+            if (!startScreen.draw && !_gameOver)
             {
                 LosingControl.Draw(spriteBatch);
                 ladybugs.Draw(spriteBatch);
             }
-            //GameOver-skjerm
-            if (gameOver) 
+            //Sluttskjerm
+            if (_gameOver) 
                 gameOverScreen.Draw(spriteBatch);
             spriteBatch.End();
             base.Draw(gameTime);
